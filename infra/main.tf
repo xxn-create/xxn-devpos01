6ef864398582a23cb56025177a16d7bb5240683c
xxn <3583487637@qq.com>
2026-08-18 11:02:37 +0800

    Initial commit: Production-Ready Microservices Platform (without large files)

---
+++ infra/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region                      = "us-east-1"
+  access_key                  = "test"
+  secret_key                  = "test"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  s3_use_path_style           = true
+
+  endpoints {
+    s3 = "http://localhost:4566"
+  }
+}
+
+resource "aws_s3_bucket" "test_bucket" {
+  bucket = "my-test-bucket-${formatdate("YYYYMMDDhhmmss", timestamp())}"
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.test_bucket.id
+}
